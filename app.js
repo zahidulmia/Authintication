a4c1a637ee8764b2c1b7006b791f584df9dae216
Zahidul Haque <zahidulmia@users.noreply.github.com>
2020-07-12 02:23:07 +0300

    simple-insertion

---
+++ app.js
@@ -0,0 +1,82 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+mongoose.connect("mongodb://localhost:27017/userdb",{useNewUrlParser: true, useUnifiedTopology: true});
+const userSchema = {
+email: String,
+password: String
+
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.use(express.static("public"));
+
+app.get("/", function (req,res){
+
+res.render("home")
+
+});
+app.get("/login", function (req,res){
+
+res.render("login")
+
+});
+app.get("/register", function (req,res){
+
+res.render("register")
+
+});
+
+
+app.post("/register", function (req,res){
+
+const newUser = new User({
+  email: req.body.username,
+  password: req.body.password
+});
+newUser.save(function(err){
+
+  if(err){
+    console.log(err);
+  }else{
+    res.render("secrets");
+  }
+});
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
